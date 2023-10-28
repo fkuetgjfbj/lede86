6e309de4c21b176f1fa86b8445ac23c24e5f4aa1
fkuetgjfbj <64370904+fkuetgjfbj@users.noreply.github.com>
2023-10-28 12:43:19 +0800

    a

--- .github/tmp/diy.sh
+++ .github/tmp/diy.sh
@@ -132,8 +132,8 @@ git clone https://github.com/sbwml/v2ray-geodata feeds/packages/net/v2ray-geodat
 # 添加额外软件包alist
 git clone https://github.com/sbwml/luci-app-alist package/alist
 sed -i 's/网络存储/存储/g' ./package/alist/luci-app-alist/po/zh-cn/alist.po
+
 rm -rf feeds/packages/lang/golang
-# svn export https://github.com/sbwml/packages_lang_golang/branches/19.x feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/lang/golang
 
 #dnsmasq
