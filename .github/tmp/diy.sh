ddf029a8fe967c4f0982ec590d24aec9e51c99ec
fkuetgjfbj <64370904+fkuetgjfbj@users.noreply.github.com>
2023-10-30 11:57:26 +0800

    a

--- .github/tmp/diy.sh
+++ .github/tmp/diy.sh
@@ -79,14 +79,25 @@ rm -rf package/network/services/dropbear
 svn export https://github.com/immortalwrt/immortalwrt/branches/openwrt-18.06-k5.4/package/network/services/dropbear ./package/network/services/dropbear
 
 # 修复 hostapd 报错
+
+# mt7921
+rm -rf package/kernel/rtl8821cu
 rm -rf package/kernel/mac80211
-# rm -rf package/kernel/mt76
+rm -rf package/kernel/mt76
 rm -rf package/network/services/hostapd
-rm package/kernel/rtw88-usb
-svn export https://github.com/DHDAXCW/lede-rockchip/trunk/package/kernel/mac80211 package/kernel/mac80211
-svn export https://github.com/DHDAXCW/lede-rockchip/trunk/package/kernel/rtw88-usb/  package/kernel/rtw88-usb
+
+svn export https://github.com/openwrt/openwrt/trunk/package/kernel/mt76 package/kernel/mt76
+svn export https://github.com/openwrt/openwrt/trunk/package/kernel/mac80211 package/kernel/mac80211
+svn export https://github.com/openwrt/openwrt/trunk/package/network/services/hostapd package/network/services/hostapd
+
+# rm -rf package/kernel/mac80211
+# rm -rf package/kernel/mt76
+# rm -rf package/network/services/hostapd
+# rm package/kernel/rtw88-usb
+# svn export https://github.com/DHDAXCW/lede-rockchip/trunk/package/kernel/mac80211 package/kernel/mac80211
+# svn export https://github.com/DHDAXCW/lede-rockchip/trunk/package/kernel/rtw88-usb/  package/kernel/rtw88-usb
 # svn export https://github.com/DHDAXCW/lede-rockchip/trunk/package/kernel/mt76 package/kernel/mt76
-svn export https://github.com/DHDAXCW/lede-rockchip/trunk/package/network/services/hostapd package/network/services/hostapd
+# svn export https://github.com/DHDAXCW/lede-rockchip/trunk/package/network/services/hostapd package/network/services/hostapd
 # cp -f  ./patch/011-fix-mbo-modules-build.patch package/network/services/hostapd/patches/011-fix-mbo-modules-build.patch
 
 # transmission web error
